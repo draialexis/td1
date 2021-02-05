bfe8e2533ec1f4ca1fe90b5724d573b3c93f3e17
Alexis DRAI <drai.alexis@gmail.com>
2021-02-05 11:59:30 +0100

    Fix ejects in ex8, flush stdin, fix 'equals' dichtmy ex6

--- main.c
+++ main.c
@@ -10,24 +10,28 @@ int main() {
 
     printf("entrez un entier\n");
     scanf("%d", &x);
+    fflush(stdin);
     printf("entrez un deuxieme entier\n");
     scanf("%d", &y);
+    fflush(stdin);
 
     int max = x;
 
     if (x < y) {
         max = y;
-    } else if (x == y) {
-        printf("Les deux sont egaux\n");
     }
-
-    printf("Le plus grand des deux est : %d\n", max);
+    if (x == y) {
+        printf("Les deux sont egaux\n");
+    } else {
+        printf("Le plus grand des deux est : %d\n", max);
+    }
 
     //ex7
     float grade;
     grade = 0;
     printf("Entrez une note\n");
     scanf("%f", &grade);
+    fflush(stdin);
     if (grade > 20) {
         printf("Le maximum possible est de 20.0\n");
     } else if (grade >= 18) {
@@ -44,6 +48,57 @@ int main() {
         printf("Echec\n");
     }
 
+    //ex8
+    char has_license;
+    has_license = 0;
+    int age;
+    age = 0;
+    int chances = 2;
+
+    printf("Bonjour, je suis Robocop. Vos papiers, s'il-vous-plait.\n");
+
+    while (chances > 0) {
+        printf("Avez-vous bien votre permis de conduire ?\n('o'ui ou 'n'on)\n");
+        scanf(" %c", &has_license);
+        fflush(stdin);
+
+        if (has_license == 'o') {
+            printf("Declarez votre age.\n(un nombre entier positif)\n");
+            scanf(" %d", &age);
+            fflush(stdin);
+
+            // scanf(" %d", &age) renvoie 0 si l'utilisateur entre autre chose qu'un entier...
+
+            if (age > 0) {
+                printf("Vous dites avoir %d ans.\n", age);
+            } else {
+                printf("Ne comprend pas ce qu'est un nombre entier positif...\n(Vous etes escorte en prison)\n");
+                break;
+            }
+
+            if (age >= 18 && age <= 120) {
+                printf("Merci. Vous pouvez y aller. Bonne journee.\n");
+                break;
+            } else if (age > 120) {
+                printf("Momie...\n(Vous etes escorte en prison)\n");
+                break;
+            } else if (age < 18) {
+                printf("Mineur...\n(Vous etes escorte en prison)\n");
+                break;
+            }
+        } else if (has_license == 'n') {
+            printf("Pas de permis...\n(Vous etes escorte en prison)\n");
+            break;
+        } else {
+            chances--;
+            if (chances <= 0) {
+                printf("Refuse de repondre 'o' ou 'n'...\n(Vous etes escorte en prison)\n");
+                break;
+            }
+            printf("Derniere chance...\n");
+        }
+    }
+
     //ex9
     float a, b, c;
     a = 0;
@@ -52,6 +107,7 @@ int main() {
 
     printf("Veuillez saisir trois reels\n");
     scanf("%f\n%f\n%f", &a, &b, &c);
+    fflush(stdin);
 
     if (a == 0 && b == 0 && c == 0) {
         printf("il y a une infinite de solutions pour a = b = c = 0\n");
@@ -73,56 +129,6 @@ int main() {
             printf("il y a deux solutions : %f et %f\n", result1, result2);
         }
     }
-
-    //ex8
-    char has_license;
-    has_license = 0;
-    int age;
-    age = 0;
-    int chances = 2;
-
-    printf("Bonjour, je suis Robocop. Vos papiers, s'il-vous-plait.\n");
-
-    while (chances > 0) {
-        printf("Avez-vous bien votre permis de conduire ?\n('o'ui ou 'n'on)\n");
-        scanf(" %c", &has_license);
-
-        if (has_license == 'o') {
-            printf("Declarez votre age.\n(un nombre entier positif)\n");
-            scanf(" %d", &age);
-
-            // scanf(" %d", &age) renvoie 0 si l'utilisateur entre autre chose qu'un entier...
-
-            if (age > 0) {
-                printf("Vous dites avoir %d ans.\n", age);
-            } else {
-                printf("Ne comprend pas ce qu'est un nombre entier positif...\n(Vous etes escorte en prison)\n");
-                return 0;
-            }
-
-            if (age >= 18 && age <= 120) {
-                printf("Merci. Vous pouvez y aller. Bonne journee.\n");
-                return 0;
-            } else if (age > 120) {
-                printf("Momie...\n(Vous etes escorte en prison)\n");
-                return 0;
-            } else if (age < 18) {
-                printf("Mineur...\n(Vous etes escorte en prison)\n");
-                return 0;
-            }
-        } else if (has_license == 'n') {
-            printf("Pas de permis...\n(Vous etes escorte en prison)\n");
-            return 0;
-        } else {
-            chances--;
-            if (chances <= 0) {
-                printf("Refuse de repondre 'o' ou 'n'...\n(Vous etes escorte en prison)\n");
-                return 0;
-            }
-            printf("Derniere chance...\n");
-        }
-    }
-
 
     return 0;
 }
